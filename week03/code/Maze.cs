625c860d547f588b9ef10f23a8e652eadd11cde5
Jojera <jojera@byupathway.edu>
2026-01-22 17:08:51 +0300

    finished weeko3 individual activity

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -33,6 +33,10 @@ public class Maze
     public void MoveLeft()
     {
         // FILL IN CODE
+        var key = (_currX, _currY);
+        if (!_mazeMap.ContainsKey(key) || !_mazeMap[key][0])
+            throw new InvalidOperationException("Can't go that way!");
+        _currX -= 1;
     }
 
     /// <summary>
@@ -42,6 +46,10 @@ public class Maze
     public void MoveRight()
     {
         // FILL IN CODE
+        var key = (_currX, _currY);
+        if (!_mazeMap.ContainsKey(key) || !_mazeMap[key][1])
+            throw new InvalidOperationException("Can't go that way!");
+        _currX += 1;
     }
 
     /// <summary>
@@ -51,6 +59,10 @@ public class Maze
     public void MoveUp()
     {
         // FILL IN CODE
+        var key = (_currX, _currY);
+        if (!_mazeMap.ContainsKey(key) || !_mazeMap[key][2])
+            throw new InvalidOperationException("Can't go that way!");
+        _currY -= 1;
     }
 
     /// <summary>
@@ -60,6 +72,10 @@ public class Maze
     public void MoveDown()
     {
         // FILL IN CODE
+        var key = (_currX, _currY);
+        if (!_mazeMap.ContainsKey(key) || !_mazeMap[key][3])
+            throw new InvalidOperationException("Can't go that way!");
+        _currY += 1;
     }
 
     public string GetStatus()
